38e657cd51fcb6d7177afb1c1e2f83eca62b2d58
ishrafil2233 <ishrafil2233@gmail.com>
2022-04-18 19:27:15 +0600

    make admin

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-// This is ishrafil branch 
-// this is dipta's branch 
 const express = require('express')
 const app = express();
 const cors = require('cors');
@@ -7,13 +5,12 @@ require('dotenv').config();
 const { MongoClient } = require('mongodb');
 const ObjectId = require('mongodb').ObjectId;
 const { query } = require('express');
-// const fileUpload = require('express-fileupload');
+const { json } = require('express/lib/response');
 
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-// app.use(fileUpload());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.y6gag.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -22,11 +19,41 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         await client.connect();
-        // console.log('database connnect successfully');
         const database = client.db('vat_office');
         const filesCollection = database.collection('files');
         const usersCollection = database.collection('users');
+        const adminCollection = database.collection('admins');
+        const receptionistCollection = database.collection('receptionist');
 
+
+        // make admin api
+        app.post("/admin", async (req, res) => {
+            const admin = req.body;
+            console.log(admin);
+            const result = await adminCollection.insertOne(admin);
+            res.json(result);
+        })
+
+        // get admin api
+        app.get("/admin", async (req, res) => {
+            const cursor = adminCollection.find({});
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+        // make receptionist api
+        app.post("/receptionist", async (req, res) => {
+            const receptionist = req.body;
+            console.log(receptionist);
+            const result = await receptionistCollection.insertOne(receptionist);
+            res.json(result);
+        })
+
+        // get receptionist api
+        app.get("/receptionist", async (req, res) => {
+            const cursor = receptionistCollection.find({});
+            const result = await cursor.toArray();
+            res.send(result);
+        })
 
         // find a single file via person email 
         app.get('/files', async (req, res) => {
@@ -113,53 +140,15 @@ async function run() {
             res.json(result);
         });
 
-        // post or add a user's photo in database 
-        // app.post('/users/photo', async (req, res) => {
-        //     const pic = req.files.image;
-        //     const picData = pic.data;
-        //     const encodedPic = picData.toString('base64')
-        //     const imageBuffer = Buffer.from(encodedPic, 'base64')
-        //     const userPhoto = { image: imageBuffer }
-        //     const result = await usersCollection.insertOne(userPhoto);
-        //     res.json(result);
-        // });
-
-        // find users department 
-
         app.get('/users', async (req, res) => {
-            // const department = req.query.department;
-            // console.log(department)
-            // const query = { department: department }
             const cursor = usersCollection.find({});
             const users = await cursor.toArray();
             res.json(users);
         });
 
-        // get receiptionist 
-        app.get('/users/:email', async (req, res) => {
-            const email = req.params.email;
-            const query = { email: email };
-            const user = await usersCollection.findOne(query);
-            let isReceiptionist = false;
-            if (user?.role === 'receptionist') {
-                isReceiptionist = true;
-            }
-            res.json({ receptionist: isReceiptionist });
-        })
-
-        // Add a receptionist 
-        app.put('/users/receptionist', async (req, res) => {
-            const user = req.body;
-            console.log('put', user)
-            const filter = { email: user.email };
-            const updateDoc = { $set: { role: 'receptionist' } };
-            const result = await usersCollection.updateOne(filter, updateDoc);
-            res.json(result);
-        })
-
     }
     finally {
-        // await client.close();
+       
     }
 }
 run().catch(console.dir);
